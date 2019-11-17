db09250654948d60f3cd8c064103f03d4adf72ff
Ashish <hangoutbuddy3@gmail.com>
2019-11-17 13:53:44 +0530

    HandlerMappings
    HandlerMappings like BeanNameUrlHandlerMapping,SimpleUrlHandlerMapping,ControllerClassNameHandlerMapping

---
+++ app12/src/com/ashsoft/controller/HelloFormController.java
@@ -0,0 +1,17 @@
+package com.ashsoft.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloFormController implements Controller {
+	
+   @Override
+ public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	   
+	 return  new ModelAndView("helloform");
+	 
+   }
+}
