6ec1a549bbf70295938d3437ef5ea4a5313e79c9
Ashish <hangoutbuddy3@gmail.com>
2019-11-19 22:02:55 +0530

    Handler Interceptor
    Handler Interceptor

---
+++ app14/src/com/ashsoft/interceptor/ProcessingTimeInterceptor.java
@@ -0,0 +1,42 @@
+package com.ashsoft.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ProcessingTimeInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		
+		long startTime=System.currentTimeMillis();
+		request.setAttribute("startTime",startTime);
+		System.out.println("===========preHandle()========");
+		return true;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView)
+			throws Exception {
+		
+		long endTime=System.currentTimeMillis();
+		long startTime=(long) request.getAttribute("startTime");
+		long processingTime = endTime-startTime;
+		modelAndView.addObject("startTime",startTime);
+		modelAndView.addObject("endTime", endTime);
+		modelAndView.addObject("processingTime",processingTime);
+		System.out.println("==============postHandle()==========");
+	}
+
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception)
+			throws Exception {
+		
+		System.out.println("============afterCompletion()=========");
+	}
+
+
+}
